5f29f82ce17da66522eb0fab524a0d305cf41d06
3dida <44850191+aallali@users.noreply.github.com>
2024-03-11 21:38:23 +0000

    [lint]

--- users/views.py
+++ users/views.py
@@ -1,6 +1,3 @@
-# views.py
-
-from django.contrib.auth import authenticate
 from rest_framework import generics, status
 from rest_framework.response import Response
 from .serializers import UserSerializer, UserLoginSerializer
