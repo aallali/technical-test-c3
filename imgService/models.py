c0fa1eff41508ce203abb5a987c025804c0f17da
3dida <44850191+aallali@users.noreply.github.com>
2024-03-13 01:04:29 +0000

    [lint]

--- imgService/models.py
+++ imgService/models.py
@@ -6,5 +6,4 @@ class Image(models.Model):
     description = models.CharField(max_length=100)
     image = models.ImageField(upload_to="images/")
     uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
-
     uploaded_at = models.DateTimeField(auto_now_add=True)
