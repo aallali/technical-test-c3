d7de60b5f3918c2918f170553cb41744434c7c37
3dida <44850191+aallali@users.noreply.github.com>
2024-03-13 00:42:10 +0000

    [fix] :  correct relation between img and user tables

--- imgService/models.py
+++ imgService/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from users.models import User
 
 
 class Image(models.Model):
     description = models.CharField(max_length=100)
     image = models.ImageField(upload_to="images/")
-    uploaded_by = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
     uploaded_at = models.DateTimeField(auto_now_add=True)
